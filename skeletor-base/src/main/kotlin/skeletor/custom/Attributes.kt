e2a42a8cb94f7d0d6a338d2cd70709c7eeef88d4
Bladymir Tellez <btellez@gmail.com>
2021-08-31 10:21:25 -0700

    Add Invert and Attribute Selector Props

--- skeletor-base/src/main/kotlin/skeletor/custom/Attributes.kt
+++ skeletor-base/src/main/kotlin/skeletor/custom/Attributes.kt
@@ -1,5 +1,6 @@
 package skeletor.custom
 
+import android.view.View
 import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.annotation.LayoutRes
@@ -7,12 +8,25 @@ import androidx.annotation.Px
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.Shimmer
 
+/**
+ * Attributes to be applied while generating the skeleton layout.
+ *
+ * @param color - ColorInt to use for the skeleton.
+ * @param cornerRadius - Float value to be used to round the corners for the skeleton.
+ * @param isShimmerEnabled - Flag to enable shimmer effect.
+ * @param shimmer - Settings for shimmer effect. See: [com.facebook.shimmer.Shimmer]
+ * @param lineSpacing - Float value that controls the lines spacing.
+ * @param invert - Boolean indicating if the skeleton colors should be inverted. Inverted skeletons color ViewGroups.
+ * @param attributesSelector - Function that allows inspection of views to customize a child view's attributes.
+ */
 sealed class Attributes {
     abstract val color: Int
     abstract val cornerRadius: Float
     abstract val isShimmerEnabled: Boolean
     abstract val shimmer: Shimmer
     abstract val lineSpacing: Float
+    open val invert: Boolean = false
+    abstract val attributesSelector: (View) -> Attributes
 }
 
 data class RecyclerViewAttributes(
@@ -23,16 +37,26 @@ data class RecyclerViewAttributes(
     override val shimmer: Shimmer,
     override val lineSpacing: Float,
     @LayoutRes val itemLayout: Int,
-    val itemCount: Int
-) : Attributes()
+    val itemCount: Int,
+    override val invert: Boolean = false,
+    private val attributesForView: ((View) -> Attributes)?
+) : Attributes() {
+    override val attributesSelector: (View) -> Attributes
+        get() = attributesForView ?: { this }
+}
 
 data class SimpleViewAttributes(
     @ColorInt override val color: Int,
     @Px override val cornerRadius: Float,
     override val isShimmerEnabled: Boolean,
     override val shimmer: Shimmer,
-    override val lineSpacing: Float
-) : Attributes()
+    override val lineSpacing: Float,
+    override val invert: Boolean = false,
+    private val attributesForView: ((View) -> Attributes)?
+) : Attributes() {
+    override val attributesSelector: (View) -> Attributes
+        get() = attributesForView ?: { this }
+}
 
 data class TextViewAttributes(
     val view: TextView,
@@ -41,5 +65,10 @@ data class TextViewAttributes(
     override val isShimmerEnabled: Boolean,
     override val shimmer: Shimmer,
     override val lineSpacing: Float,
-    val length: Int
-) : Attributes()
+    val length: Int,
+    override val invert: Boolean = false,
+    private val attributesForView: ((View) -> Attributes)?
+) : Attributes() {
+    override val attributesSelector: (View) -> Attributes
+        get() = attributesForView ?: { this }
+}
